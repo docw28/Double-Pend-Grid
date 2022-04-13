230cbae77a42469c49a5e1b03733db5d94a87ef2
docw28 <d.carverwilliams@gmail.com>
2022-04-13 21:57:03 +0100

    initialised repo

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => consol.log(`listening at ${port}`));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb"}));
