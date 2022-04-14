6d0cd69088d2f62643e19c34ccdadb57461fd54f
docw28 <d.carverwilliams@gmail.com>
2022-04-14 01:02:49 +0100

    its done

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => consol.log(`listening at ${port}`));
-app.use(express.static("public"));
-app.use(express.json({ limit: "1mb"}));
